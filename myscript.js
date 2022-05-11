879e766d22e42685e816d820d9460922d4b8869b
Zainab Ayubgit config --global user.email zainabayub97@gmail.comgit config --global user.name Your <zainabayub97@gmail.com>
2022-05-11 16:47:40 +1000

    create game function and scoreboard

--- myscript.js
+++ myscript.js
@@ -1,34 +1,71 @@
-const computerSelection = computerPlay();
-const playerSelection = prompt('What is your choice?')
+
+let playerScore = 0
+let computerScore = 0
+game();
+
+
+
+
 
 
 function computerPlay() {
     let options = ['rock', 'paper', 'scissors'];
     return options[Math.floor(Math.random()*options.length)];
 }
-console.log("Computer chose" + " " + computerSelection);
-console.log("You chose" + " " + playerSelection);
+
 
 function playRound(playerSelection, computerSelection) {
 if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock') {
-   return 'It is a tie!'
+   alert('It is a tie!')
 } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
-    return 'You lose! Paper Beats Rock!'
+    computerScore++
+    alert('You lose! Paper Beats Rock!')
 } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
-    return 'You win! Rock beats Scissors!'
+    playerScore++
+    alert('You win! Rock beats Scissors!')
 } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
-    return 'You win! Paper beats Rock!'
+    playerScore++
+    alert('You win! Paper beats Rock!')
 } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper') {
-    return 'It is a tie!'
+    alert('It is a tie!')
 } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') {
-    return 'You lose! Scissors beats paper!'
+    computerScore++
+    alert('You lose! Scissors beats paper!')
 } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
-   return 'You lose! Rock beats scissors!'
+   computerScore++
+    alert('You lose! Rock beats scissors!')
 } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
-   return 'You win! Scissors beats paper!'
+   playerScore++
+   alert('You win! Scissors beats paper!')
 } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'scissors') {
-    console.log('It is a tie!');
+   alert('It is a tie!')
+} else {
+    return 'What was that?'
 }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+    const computerSelection = computerPlay();
+    const playerSelection = prompt('What is your choice?')
+    console.log("Computer chose" + " " + computerSelection);
+    console.log("You chose" + " " + playerSelection);
+    playRound(playerSelection, computerSelection);
+    }
+}
+    console.log("Game over")
+    if(playerScore > computerScore) {
+        console.log("You won the game!")
+    } else if (playerScore == computerScore) {
+        console.log("It is a Tie!")
+    } else {
+        console.log("You lost the game! Computer wins this time.");
+    }
+
+
+
+
+
+
